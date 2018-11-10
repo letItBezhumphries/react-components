07ef75393ad0f1671efd4183363513dfaf136e93
EricZH <letItBezhumphries>
2018-11-10 15:09:56 -0800

    Complete implementing GroceryListItem component

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-function ListItem(props) {
-  return <li>{props.item}</li>;
-}; 
 
-var GroceryList = () => (
-  <div> 
-    <h1> My Badass & Simple Grocery list </h1> 
-    <ul>
-      <ListItem item="Maple Syrup"/>
-      <ListItem item="Waffle Maker"/>
-      <ListItem item="Orange Juice"/>
-      <ListItem item="Fried-French Toast"/>
-    </ul>
-  </div>
+function ListItem(props) {
+  return <li>{props.value}</li>
+}
+
+function GroceryList(props) {
+  const items = props.items;
+  const listItems = items.map((item) =>
+    <ListItem key={item.toString()} value={item}/>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const items = ["Maple Syrup", "Waffle Maker", "Orange Juice", "Fried French Toast", "Advil"];
+ReactDOM.render(
+  <GroceryList items={items} />,
+  document.getElementById('app')
 );
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
+
+
+
+
+
