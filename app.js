24d6ef9d5c87d715a74c09d7cb0664d3c1883064
EricZH <letItBezhumphries>
2018-11-09 21:52:03 -0800

    Complete creating the grocery list component

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// TODO
+var GroceryList = () => (
+  <div>  
+    <ul>
+      <li>Maple Syrup</li>
+      <li>Orange Juice</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
